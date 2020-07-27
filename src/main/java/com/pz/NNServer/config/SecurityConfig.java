0746b8db115c135c122a0857c7c0c4df616dd9e7
K1C4Y <kicaj565@gmail.com>
2020-07-27 18:18:22 +0200

    Added security config.

---
+++ src/main/java/com/pz/NNServer/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.pz.NNServer.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecuruty) throws Exception{
+       httpSecuruty.csrf().disable()
+               .authorizeRequests()
+               .antMatchers("/api/auth/**")
+               .permitAll()
+               .anyRequest()
+               .authenticated();
+    }
+}
